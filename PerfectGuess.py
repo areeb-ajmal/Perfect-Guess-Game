8c33f478bc2082d12d0a5b0666cb5c01ec23807b
Areeb Ajmal <igxareeb@gmail.com>
2025-10-02 17:20:11 +0500

    PerfectGuess.py

--- PerfectGuess.py
+++ PerfectGuess.py
@@ -2,19 +2,21 @@ import random
 
 n = random.randint(1, 100)
 a = -1
-guesses = 0
+guesses = 1
 
 while a != n :
-    guesses += 1
     a = int(input("Enter your Number : "))
 
     if a > n :
         print("Enter Lower number please!")
+        guesses += 1
 
-    else :
+    elif (a < n):
         print("Enter Higher number please!")
+        guesses += 1
 
 print(f"Congratulations you have guessed {n} in {guesses} attepmts")
+
 
 
 
